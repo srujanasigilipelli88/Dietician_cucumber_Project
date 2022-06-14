0ba44d6cb45ec326fcef511b43b64fcfeb319f8a
Sruja <Sruja@GWTN156-11.lan>
2022-06-13 23:02:08 -0400

    .

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features",glue={"stepDefinitions"},
+@CucumberOptions(features="src/test/resources/features",glue={"stepdefinitions"},
 monochrome = true,
 plugin={"pretty","html:taget/HtmlReports.html","json:target/JSONReports/reports.json","junit:target/JUnitReports/report.xml"})
 
